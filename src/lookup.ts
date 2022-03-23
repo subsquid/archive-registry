8c00d8554278e3adacce78c1c3b0de79843ecb94
dzhelezov <dzhelezov@gmail.com>
2022-03-23 16:40:46 +0100

    rename name -> provider

--- src/lookup.ts
+++ src/lookup.ts
@@ -1,8 +1,8 @@
 import fs from "fs"
 import fetch from "node-fetch"
+import { AbortController } from "node-abort-controller";
 import assert from "assert"
-import { Network } from './intefaces'
-import { ArchiveRegistry, NetworkRegistry } from "."
+import { Network, ArchiveRegistry, NetworkRegistry } from "."
 
 
 export const networkRegistry = JSON.parse(fs.readFileSync(`${__dirname}/../networks.json`).toString()) as NetworkRegistry
@@ -17,7 +17,7 @@ export const archivesRegistry = JSON.parse(fs.readFileSync(`${__dirname}/../regi
 export function lookupArchive(
     network: string,
     genesis?: string,
-    provider?: string): string {
+    provider?: string): { url: string, version: string } {
     
     let archives = archivesRegistry.archives.filter(a => a.network.toLowerCase() === network.toLowerCase())
     if (genesis) {
@@ -36,10 +36,10 @@ Please consider submitting a PR to subsquid/archive-registry github repo to exte
             throw new Error(`Failed to lookup a matching archive. \
 Please consider submitting a PR to subsquid/archive-registry github repo to extend the registry`)
         }
-        return filtered[0].url
+        return filtered[0]
     }
     // take the first one by default (which is subsquid)
-    return archive.providers[0].url
+    return archive.providers[0]
 }
 
 /**
@@ -59,15 +59,34 @@ Please consider submitting a PR to subsquid/archive-registry github repo to exte
 
 export async function getGenesisHash(endpoint: string): Promise<string> {
     const query = `
+    query {
         substrate_block(where: {height: {_eq: 0}}) {
-           hash
+            hash
         }
+    }
     `
-    const result = await archiveRequest<{hash: string}[]>(endpoint, query)
-    return result[0].hash
+    const result = await archiveRequest<{ substrate_block: {hash: string}[] }>(endpoint, query)
+    return result.substrate_block[0].hash
 }  
 
+export async function getVersion(endpoint: string): Promise<string> {
+    const query = `
+    query {
+        indexerStatus {
+            hydraVersion
+        }
+    }
+    `
+    const result = await archiveRequest<{indexerStatus: { hydraVersion: string }}>(endpoint, query)
+    return result.indexerStatus.hydraVersion
+}
+
+
 async function archiveRequest<T>(endpoint: string, query: string): Promise<T> {
+    const controller = new AbortController()
+    // 5 second timeout:
+    const timeoutId = setTimeout(() => controller.abort(), 5000)
+
     let response = await fetch(endpoint, {
         method: 'POST',
         body: JSON.stringify({query}),
@@ -75,8 +94,11 @@ async function archiveRequest<T>(endpoint: string, query: string): Promise<T> {
             'content-type': 'application/json',
             'accept': 'application/json',
             'accept-encoding': 'gzip, br'
-        }
+        },
+        signal: controller.signal
     })
+    clearTimeout(timeoutId)
+
     if (!response.ok) {
         let body = await response.text()
         throw new Error(`Got http ${response.status}${body ? `, body: ${body}` : ''}`)
