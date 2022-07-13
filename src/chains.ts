3e7a0a3eff93fc7cc8ef12836f41e908ef57086d
Nikita Klimin <nklimin007@gmail.com>
2022-07-13 19:13:16 +0300

    Add hydradx archive (#19)
    * feat: add types and archive description
    
    * chore: fix typo
    
    * feat: update networks descriptions

--- src/chains.ts
+++ src/chains.ts
@@ -4,6 +4,6 @@
 // Run npm run gen-types to generate this file from archives.
 //
 
-export type KnownArchives = "equilibrium"|"moonbeam"|"acala"|"kusama"|"polkadot"|"karura"|"crust"|"bifrost"|"moonriver"|"astar"|"shibuya"|"hydradx-snakenet"|"subsocial-solochain"|"quartz"|"moonbase"|"phala"
+export type KnownArchives = "equilibrium"|"moonbeam"|"acala"|"kusama"|"polkadot"|"karura"|"crust"|"bifrost"|"moonriver"|"astar"|"shibuya"|"hydradx-snakenet"|"subsocial-solochain"|"quartz"|"moonbase"|"phala"|"hydradx"
 export type KnownArchivesV5 = "calamari"|"pioneer"|"statemint"|"parallel"|"moonbeam"|"clover"|"nodle"|"quartz"|"heiko"|"kintsugi"|"basilisk"|"altair"|"acala"|"kusama"|"polkadot"|"karura"|"crust"|"bifrost"|"statemine"|"khala"|"kilt"|"moonriver"|"astar"|"shiden"|"hydradx"|"robonomics"|"subsocial"|"moonbase"
 
