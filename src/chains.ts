3a95070038cfb6c69aa47a92f4323f70777d8ac0
Nikita Klimin <nklimin007@gmail.com>
2023-01-04 13:35:10 +0300

    feat: add arbitrum

--- src/chains.ts
+++ src/chains.ts
@@ -5,6 +5,6 @@
 //
 
 export type KnownArchivesSubstrate = "acala"|"aleph-zero-testnet"|"astar"|"bajun"|"bifrost"|"calamari"|"crust"|"efinity"|"equilibrium"|"gear-testnet"|"gemini-2a-testnet"|"gmordie"|"hydradx"|"interlay"|"invarch-tinkernet"|"karura"|"khala"|"kintsugi"|"kusama"|"litentry"|"litmus"|"moonbase"|"moonbeam"|"moonriver"|"opal"|"peaq"|"phala"|"polkadot"|"quartz"|"reef"|"reef-testnet"|"rococo"|"shibuya"|"shiden"|"snow"|"statemine"|"statemint"|"subsocial-parachain"|"unique"
-export type KnownArchivesEVM = "avalanche"|"binance"|"binance-testnet"|"eth-l2"|"eth-mainnet"|"exosama"|"fantom"|"goerli"|"polygon"|"polygon-mumbai"
+export type KnownArchivesEVM = "avalanche"|"arbitrum"|"binance"|"binance-testnet"|"eth-l2"|"eth-mainnet"|"exosama"|"fantom"|"goerli"|"polygon"|"polygon-mumbai"
 export type KnownArchives = KnownArchivesSubstrate | KnownArchivesEVM
 
