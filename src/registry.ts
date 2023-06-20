75840f3a2648795a74c688e7b4529886de6212d8
Alexander Belopashentsev <abelopashentsev@gmail.com>
2023-06-20 13:20:32 +0500

    use cdn links

--- src/registry.ts
+++ src/registry.ts
@@ -2,13 +2,13 @@ import fetch from 'sync-fetch'
 import {ArchiveRegistrySubstrate, ArchiveRegistryEVM, NetworkRegistrySubstrate} from './intefaces'
 
 export function networkRegistrySubstrate(): NetworkRegistrySubstrate {
-    return fetch('https://raw.githubusercontent.com/subsquid/archive-registry/main/networks.json').json()
+    return fetch('https://cdn.subsquid.io/archives/networks.json').json()
 }
 
 export function archivesRegistrySubstrate(): ArchiveRegistrySubstrate {
-    return fetch('https://raw.githubusercontent.com/subsquid/archive-registry/main/archives.json').json()
+    return fetch('https://cdn.subsquid.io/archives/substrate.json').json()
 }
 
 export function archivesRegistryEVM(): ArchiveRegistryEVM {
-    return fetch('https://raw.githubusercontent.com/subsquid/archive-registry/main/archives-evm.json').json()
+    return fetch('https://cdn.subsquid.io/archives/evm.json').json()
 }
