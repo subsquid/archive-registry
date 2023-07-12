c4bcd1fc8d588975d80f44cd9617ddfa15670e77
Alexander Belopashentsev <abelopashentsev@gmail.com>
2023-07-13 02:14:41 +0500

    feat: allow to filter archive list by release

--- src/commands/list.ts
+++ src/commands/list.ts
@@ -7,52 +7,68 @@ runProgram(async () => {
     let program = new Command()
 
     program.description(`Display list of available archives`)
-    program.addOption(new Option(`-t --type <type>`, `Network type`).choices([`evm`, `substrate`]))
+    program.addOption(new Option(`-t --type <string>`, `Network type`).choices([`evm`, `substrate`]))
+    program.addOption(new Option(`-r --release <string>`, `Release name`).choices([`FireSquid`, `ArrowSquid`]))
 
     program.parse()
-    let opts: {type: 'substrate' | 'evm'} = program.opts()
+    let opts: {
+        type?: 'substrate' | 'evm'
+        release?: 'FireSquid' | 'ArrowSquid'
+    } = program.opts()
+
+    const filter: Filter = {release: opts.release}
 
     switch (opts.type) {
         case 'evm':
-            printEvmArchives()
+            printEvmArchives(filter)
             break
         case 'substrate':
-            printSubstrateArchives()
+            printSubstrateArchives(filter)
             break
         case null:
         case undefined:
-            printEvmArchives()
-            printSubstrateArchives()
+            printEvmArchives(filter)
+            printSubstrateArchives(filter)
             break
     }
 })
 
-function printEvmArchives() {
+interface Filter {
+    release?: string
+}
+
+function printEvmArchives(filter: Filter) {
     console.log(`EVM archives:`)
     let table = new Table()
     table.pushDelimeter(['network', 'release', 'endpoint'])
     for (let archive of archivesRegistryEVM().archives) {
         table.cell('network', archive.network)
         for (let provider of archive.providers) {
+            if (filter.release != null && filter.release != provider.release) continue
+
             table.cell('release', provider.release)
             table.cell('endpoint', provider.dataSourceUrl)
             table.newRow()
         }
     }
+
     console.log(table.print())
 }
 
-function printSubstrateArchives() {
+function printSubstrateArchives(filter: Filter) {
     console.log(`Substrate archives:`)
     let table = new Table()
     table.pushDelimeter(['network', 'release', 'endpoint'])
     for (let archive of archivesRegistrySubstrate().archives) {
         table.cell('network', archive.network)
         for (let provider of archive.providers) {
+            if (filter.release != null && filter.release != provider.release) continue
+
             table.cell('release', provider.release)
             table.cell('endpoint', provider.dataSourceUrl)
             table.newRow()
         }
     }
+
     console.log(table.print())
 }
