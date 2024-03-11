bc2c0fed66a83c46293fa80fa52118a3352973d5
octo-gone <nklimin007@gmail.com>
2024-03-11 15:06:30 +0300

    fix: format code and remove deprecated info fields

--- src/commands/list.ts
+++ src/commands/list.ts
@@ -1,74 +1,86 @@
-import {Command, Option} from 'commander'
-import Table from 'easy-table'
-import {runProgram} from '@subsquid/util-internal'
-import {archivesRegistryEVM, archivesRegistrySubstrate} from '../registry'
+import { Command, Option } from "commander";
+import Table from "easy-table";
+import { runProgram } from "@subsquid/util-internal";
+import { archivesRegistryEVM, archivesRegistrySubstrate } from "../registry";
 
 runProgram(async () => {
-    let program = new Command()
+    let program = new Command();
 
-    program.description(`Display list of available archives`)
-    program.addOption(new Option(`-t --type <string>`, `Network type`).choices([`evm`, `substrate`]))
-    program.addOption(new Option(`-r --release <string>`, `Release name`).choices([`FireSquid`, `ArrowSquid`]))
+    program.description(`Display list of available archives`);
+    program.addOption(
+        new Option(`-t --type <string>`, `Network type`).choices([
+            `evm`,
+            `substrate`,
+        ])
+    );
+    program.addOption(
+        new Option(`-r --release <string>`, `Release name`).choices([
+            `FireSquid`,
+            `ArrowSquid`,
+        ])
+    );
 
-    program.parse()
+    program.parse();
     let opts: {
-        type?: 'substrate' | 'evm'
-        release?: 'FireSquid' | 'ArrowSquid'
-    } = program.opts()
+        type?: "substrate" | "evm";
+        release?: "FireSquid" | "ArrowSquid";
+    } = program.opts();
 
-    const filter: Filter = {release: opts.release}
+    const filter: Filter = { release: opts.release };
 
     switch (opts.type) {
-        case 'evm':
-            printEvmArchives(filter)
-            break
-        case 'substrate':
-            printSubstrateArchives(filter)
-            break
+        case "evm":
+            printEvmArchives(filter);
+            break;
+        case "substrate":
+            printSubstrateArchives(filter);
+            break;
         case null:
         case undefined:
-            printEvmArchives(filter)
-            printSubstrateArchives(filter)
-            break
+            printEvmArchives(filter);
+            printSubstrateArchives(filter);
+            break;
     }
-})
+});
 
 interface Filter {
-    release?: string
+    release?: string;
 }
 
 function printEvmArchives(filter: Filter) {
-    console.log(`EVM archives:`)
-    let table = new Table()
-    table.pushDelimeter(['network', 'release', 'endpoint'])
+    console.log(`EVM archives:`);
+    let table = new Table();
+    table.pushDelimeter(["network", "release", "endpoint"]);
     for (let archive of archivesRegistryEVM().archives) {
-        table.cell('network', archive.network)
+        table.cell("network", archive.network);
         for (let provider of archive.providers) {
-            if (filter.release != null && filter.release != provider.release) continue
+            if (filter.release != null && filter.release != provider.release)
+                continue;
 
-            table.cell('release', provider.release)
-            table.cell('endpoint', provider.dataSourceUrl)
-            table.newRow()
+            table.cell("release", provider.release);
+            table.cell("endpoint", provider.dataSourceUrl);
+            table.newRow();
         }
     }
 
-    console.log(table.print())
+    console.log(table.print());
 }
 
 function printSubstrateArchives(filter: Filter) {
-    console.log(`Substrate archives:`)
-    let table = new Table()
-    table.pushDelimeter(['network', 'release', 'endpoint'])
+    console.log(`Substrate archives:`);
+    let table = new Table();
+    table.pushDelimeter(["network", "release", "endpoint"]);
     for (let archive of archivesRegistrySubstrate().archives) {
-        table.cell('network', archive.network)
+        table.cell("network", archive.network);
         for (let provider of archive.providers) {
-            if (filter.release != null && filter.release != provider.release) continue
+            if (filter.release != null && filter.release != provider.release)
+                continue;
 
-            table.cell('release', provider.release)
-            table.cell('endpoint', provider.dataSourceUrl)
-            table.newRow()
+            table.cell("release", provider.release);
+            table.cell("endpoint", provider.dataSourceUrl);
+            table.newRow();
         }
     }
 
-    console.log(table.print())
+    console.log(table.print());
 }
--- src/registry.ts
+++ src/registry.ts
@@ -1,14 +1,18 @@
-import fetch from 'sync-fetch'
-import {ArchiveRegistrySubstrate, ArchiveRegistryEVM, NetworkRegistrySubstrate} from './interfaces'
+import fetch from "sync-fetch";
+import {
+    ArchiveRegistrySubstrate,
+    ArchiveRegistryEVM,
+    NetworkRegistrySubstrate,
+} from "./interfaces";
 
 export function networkRegistrySubstrate(): NetworkRegistrySubstrate {
-    return fetch('https://cdn.subsquid.io/archives/networks.json').json()
+    return fetch("https://cdn.subsquid.io/archives/networks.json").json();
 }
 
 export function archivesRegistrySubstrate(): ArchiveRegistrySubstrate {
-    return fetch('https://cdn.subsquid.io/archives/substrate.json').json()
+    return fetch("https://cdn.subsquid.io/archives/substrate.json").json();
 }
 
 export function archivesRegistryEVM(): ArchiveRegistryEVM {
-    return fetch('https://cdn.subsquid.io/archives/evm.json').json()
+    return fetch("https://cdn.subsquid.io/archives/evm.json").json();
 }
